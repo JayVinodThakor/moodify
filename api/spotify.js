7255bc2a1daddd597712d0888f6ebd6dd5de2808
JayVinodThakor <jayvinodthakor@gmail.com>
2026-04-24 01:24:19 +0530

    Add files via upload

---
+++ api/spotify.js
@@ -0,0 +1,34 @@
+export default async function handler(req, res) {
+  const clientId = process.env.SPOTIFY_CLIENT_ID;
+  const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+  const auth = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
+
+  // Get token
+  const tokenRes = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${auth}`,
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "grant_type=client_credentials"
+  });
+
+  const tokenData = await tokenRes.json();
+
+  const mood = req.query.mood || "happy";
+
+  // Search playlist
+  const dataRes = await fetch(
+    `https://api.spotify.com/v1/search?q=${mood}&type=playlist&limit=1`,
+    {
+      headers: {
+        Authorization: `Bearer ${tokenData.access_token}`
+      }
+    }
+  );
+
+  const data = await dataRes.json();
+
+  res.status(200).json(data);
+}
